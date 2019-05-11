b319118e17611af9fb2bff8383077266e35c0817
san06vicky <50006383+san06vicky@users.noreply.github.com>
2019-05-11 17:31:24 -0500

    Update HelloWorld.java
    changed

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,12 @@
 
 public class HelloWorld extends SuperClass {
-	public int x;
+	
 	
 	public static void main(String[] arg) {
 	
 		
-		int b = s.indexOf('?');
-		System.out.println("b value:"+b);
+		
+		System.out.println("Hello World");
 	}
 
 }
