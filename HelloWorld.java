b2cba380eed4d7679d16fce2a76d39a3cba1c16f
san06vicky <50006383+san06vicky@users.noreply.github.com>
2019-05-11 17:05:22 -0500

    Update HelloWorld.java
    trying branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,55 +3,8 @@ public class HelloWorld extends SuperClass {
 	public int x;
 	
 	public static void main(String[] arg) {
-	/*	String[] list = {"Facebook","Forgot account?","Why do I need to provide my birthday?,"
-						,"Terms","Data Policy","Cookie Use","","","Create a Page","Español"
-						,"Français (France)"	,"??(??)","???????","Português (Brasil)","Italiano"
-						,"???"		,"Deutsch"		,"??????","???"
-						,""
-						,"Sign Up"
-						,"Log In"
-						,"Messenger"
-						,"Facebook Lite"
-						,"Mobile"
-						,"Find Friends"
-						,"People"
-						,"Pages"
-						,"Places"
-						,"Games"
-						,"Locations"
-						,"Celebrities"
-						,"Marketplace"
-						,"Groups"
-						,"Recipes"
-						,"Sports"
-						,"Look"
-						,"Moments"
-						,"Instagram"
-						,"Local"
-						,"About"
-						,"Create Ad"
-						,"Create Page"
-						,"Developers"
-						,"Careers"
-						,"Privacy"
-						,"Cookies"
-						,"Ad Choices"
-						,"Terms"
-						,"Help"
-						,"Settings"
-						,"Activity Log"
-						};
-		for (String linkTemp : list) {
-
-		  
-			   boolean b = linkTemp.contains("?");
-			   System.out.println(linkTemp+"has ?"+b);
-			   if ((linkTemp != "") && (!b)) {
-				     System.out.println("\nClicking on Link: " + linkTemp);
-			   }
-		}
-*/
-		String s =" ??????";
+	
+		
 		int b = s.indexOf('?');
 		System.out.println("b value:"+b);
 	}
